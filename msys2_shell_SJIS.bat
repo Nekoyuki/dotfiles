eaaf76aedf4bb9d3a3134a91d9e2885672bb0192
Nekoyuki <hiyokommm@gmail.com>
2016-04-12 23:15:14 +0900

    new msys2_shell_xxx.bat for SJIS, UTF-8

---
+++ msys2_shell_SJIS.bat
@@ -0,0 +1,17 @@
+:
+@echo off
+
+rem To activate windows native symlinks uncomment next line
+rem set MSYS=winsymlinks:nativestrict
+
+rem Set debugging program for errors
+rem set MSYS=error_start:%WD%../../mingw32/bin/qtcreator.exe^|-debug^|^<process-id^>
+
+rem To export full current PATH from environment into MSYS2 uncomment next line
+rem set MSYS2_PATH_TYPE=inherit
+
+set "WD=%__CD__%"
+if NOT EXIST "%WD%msys-2.0.dll" set "WD=%~dp0usr\bin\"
+set CONTITLE=MSys2_SJIS
+start "%CONTITLE%" "%WD%mintty" -c my_minttyrc_SJIS -i /msys2.ico /usr/bin/zsh --login %1 %2 %3 %4 %5 %6 %7 %8 %9
+:EOF
--- setup.sh
+++ setup.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 # ln -sf ~/dotfiles/my_vimrc ~/.vimrc
-ln -sf ~/dotfiles/my_minttyrc ~/.minttyrc
+# ln -sf ~/dotfiles/my_minttyrc ~/.minttyrc
 ln -sf ~/dotfiles/my_vimshrc ~/.vimshrc
+
+case ${OSTYPE} in
+    linux*)
+        ;;
+    msys*)
+        ln -sf ~/dotfiles/my_minttyrc_UTF-8 /c/msys64/my_minttyrc_UTF-8
+        ln -sf ~/dotfiles/my_minttyrc_SJIS /c/msys64//my_minttyrc_SJIS
+        ;;
+esac
